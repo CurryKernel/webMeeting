0b2b0cc48c30fee675ede9f2b8487e0eb155ffbe
KD1219 <1289735075@qq.com>
2020-12-14 14:06:13 +0800

    用户功能增添修改信息，日历功能完善。用户界面图片更换

--- task/src/Service/UserService.java
+++ task/src/Service/UserService.java
@@ -15,4 +15,19 @@ public class UserService {
     public List<User> findAll(){
         return user.findAll();
     }
+    //查全部用户的信息，分页
+    public List<User> findAll(int pageId, int pageSize){
+        return user.findAll(pageId,pageSize);
+    }      //根据用户ID修改密码
+    public void updatePasswordById(String id, String password){
+        user.updatePasswordById(id,password);
+    }
+    //总记录数
+    public int count(){
+        return user.count();
+    }
+    //根据用户ID一部分查询全部用户
+    public List<User> findByPartOfUserId(String partId){
+        return user.findByPartOfUserId(partId);
+    }
 }
